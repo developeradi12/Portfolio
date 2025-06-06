a794e58ebe6d76e950558ff83f8de1a567a8b683
developeradi12 <adityagangil182@gmail.com>
2025-06-06 12:39:38 +0530

    aboutedit

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -3,8 +3,7 @@ import { useRef } from "react";
 import Card from "../components/Card"
 import { Globe } from "../components/globe";
 import CopyEmailButton from '../components/CopyEmailButton';
-import { Frameworks } from "../components/FrameWorks";
-
+import { Frameworks } from "../components/Frameworks"
 const About = ({ id }) => {
     const grid2Container = useRef();
     return (
